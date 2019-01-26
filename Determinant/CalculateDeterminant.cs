fc314934b17d1f52c94fcad37f97f18a53bbafff
Leonov Artem <leonovru@yandex.ru>
2019-01-26 20:02:36 +0300

    Add method 'Minor' and tests in Program.cs

--- Determinant/CalculateDeterminant.cs
+++ Determinant/CalculateDeterminant.cs
@@ -8,13 +8,33 @@ namespace Determinant
 {
     class CalculateDeterminant
     {
-        private double[] matrix;
+        private double[,] matrix;
         private int Dimension { get; set; }
 
-        public CalculateDeterminant(double[] matrix)
+        public CalculateDeterminant(double[,] matrix)
         {
             this.matrix = matrix;
-            Dimension = matrix.Rank;
+            Dimension = matrix.GetLength(0);
+        }
+
+        public double[,] Minor(double[,] matrix, int x, int y)
+        {
+            double[,] minor = new double[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+
+            for (int row = 0, i = 0; row < matrix.GetLength(1); row++)
+            {
+                if (row != x)
+                {
+                    for (int col = 0, j = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (col != y)
+                            minor[i, j++] = matrix[row, col];
+                    }
+                    i++;
+                }
+            }
+
+            return minor;
         }
     }
 }
--- Determinant/Program.cs
+++ Determinant/Program.cs
@@ -10,6 +10,38 @@ namespace Determinant
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int dim = random.Next(3, 6);
+
+            double[,] matrix = new double[dim, dim];
+            FillMatrix(matrix);
+            Print(matrix);
+            Console.WriteLine();
+
+            CalculateDeterminant determinant = new CalculateDeterminant(matrix);
+            double[,] minor = determinant.Minor(matrix, 0, 0);
+            Print(minor);
+
+            Console.ReadKey();
+        }
+        static void FillMatrix(double[,] matrix)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = random.Next(-13, 19); 
+            }
+            Console.WriteLine();
+        }
+        static void Print(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write("{0,3} ", matrix[i, j]);
+                Console.WriteLine();
+            }
         }
     }
 }
